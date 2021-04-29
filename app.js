fea234c9f4ae8864bdb39557eec69b2d226f7b93
Hein Jeong <hein.jeong@gmail.com>
2021-04-29 15:27:11 -0700

    Add helmet middleware

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const foodsRouter = require('./routes/foods');
 const apiKeyRouter = require('./routes/apikey');
 const authentication = require('./middleware/authentication');
 const connectDB = require('./config/connectDB');
+const helmet = require("helmet");
 
 connectDB();
 
 const app = express();
 
+app.use(helmet());
+
 app.use(express.json());
 
-
 app.use(['/foods', '/apikey'], authentication);
+
 app.use('/foods', foodsRouter);
+
 app.use('/apikey', apiKeyRouter);
 
 module.exports = app;
